7b5023b42bfc4ac389133fba24fd6c621ee41675
YasminPilla <yasmineduarda289@gmail.com>
2026-03-02 23:35:12 -0300

    fix: CORS github pages + URL produção

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://ysxmni.github.io",
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "http://127.0.0.1:5501",
