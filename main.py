61505290c5d88c7454a2b4e4bac13928d08640fa
YasminPilla <yasmineduarda289@gmail.com>
2026-03-03 22:32:38 -0300

    feat: Ajuste no Cors

--- main.py
+++ main.py
@@ -63,17 +63,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://ysxmni.github.io",
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "http://127.0.0.1:5501",
-        "http://localhost:5501",
-        "http://127.0.0.1:8080",
-        "http://localhost:8080",
-        "null",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
